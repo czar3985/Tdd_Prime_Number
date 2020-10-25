d1e2e87fedb35b6fbb4987d9b21fdedbdbea9b88
Rina Lingad <pixie.czar@gmail.com>
2020-10-25 14:25:14 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tdd_Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
