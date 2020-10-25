6c45d6464464b6cb307dd8db5d1a2e0ff0acf88e
Rina Lingad <pixie.czar@gmail.com>
2020-10-25 16:01:33 +1300

    Fix code to handle edge cases

--- Tdd_Prime_Number/PrimeNumberChecker.cs
+++ Tdd_Prime_Number/PrimeNumberChecker.cs
@@ -5,6 +5,10 @@
         public bool IsPrime(int number)
         {
             // Prime Number: a number that is divisible only by itself and 1.
+            if (number < 2)
+            {
+                return false;
+            }
             for (var divisor = 2; divisor < number; divisor++)
             {
                 if (number % divisor == 0)
